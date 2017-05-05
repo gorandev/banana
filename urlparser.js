45d0e230c77ecd0f8da2a2a1bed9725f975455e4
Joaquin Perez <mondongo@gmail.com>
2017-05-05 16:05:54 -0300

    Moved some of the code to functions to make it more legible.

--- urlparser.js
+++ urlparser.js
@@ -6,33 +6,39 @@ console.log(url_parser(url_format_string, url_instance));
 function url_parser(url_format_string, url_instance) {
   var parsedOutput = {};
 
-  url_format_string_arr = url_format_string.split('/');
-  url_instance_arr = url_instance.split('?');
-  url_instance_string_arr = url_instance_arr[0].split('/');
+  var url_format_string_arr = url_format_string.split('/');
+  var url_instance_arr = url_instance.split('?');
+  var url_instance_string_arr = url_instance_arr[0].split('/');
 
   for (x = 0; x < url_format_string_arr.length; x++) {
     if (url_format_string_arr[x].charAt(0) == ':') {
-      var val = isNaN(Number(url_instance_string_arr[x])) ? url_instance_string_arr[x] : Number(url_instance_string_arr[x]);
-      parsedOutput[url_format_string_arr[x].substring(1)] = val
+      parsedOutput[url_format_string_arr[x].substring(1)] = number_if_number(url_instance_string_arr[x])
     }
   }
 
   if (url_instance_arr.length > 1) {
     var arr = url_instance_arr[1].split('&');
     for (x = 0; x < arr.length; x++) {
-      var spl = arr[x].split('=');
-      var val = isNaN(Number(spl[1])) ? spl[1] : Number(spl[1]);
-      if (spl.length == 2) {
-        if (typeof parsedOutput[spl[0]] === 'array') {
-          parsedOutput[spl[0]].push(val);
-        } else if (typeof parsedOutput[spl[0]] === 'string') {
-          parsedOutput[spl[0]] = [parsedOutput[spl[0]], val]
-        } else {
-          parsedOutput[spl[0]] = val;
-        }
+      [key, val] = arr[x].split('=');
+      if (key && val) {
+        save_as_array_if_existing(parsedOutput, key, number_if_number(val));
       }
     }
   }
 
   return parsedOutput;
+}
+
+function number_if_number(val) {
+  return isNaN(Number(val)) ? val : Number(val);
+}
+
+function save_as_array_if_existing(hash, key, val) {
+  if (typeof hash[key] === 'array') {
+    hash[key].push(val);
+  } else if (typeof hash[key] === 'string') {
+    hash[key] = [ hash[key], val ];
+  } else {
+    hash[key] = val;
+  }
 }
